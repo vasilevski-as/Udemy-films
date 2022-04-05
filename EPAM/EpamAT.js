bdef3c635a913981bb3d540e941660e2197840d3
Vasilevski.as <vasilevski.as@gmail.com>
2022-04-06 00:36:19 +0300

    2 ex

---
+++ EPAM/EpamAT.js
@@ -0,0 +1,43 @@
+// 1 перебор массива с выводом в консоль каждого элемента
+let arrayFilms = ['Ведьмак', 'Довод', 'Человек паук', 'Матрица',
+ 'Тьма', 'Сноуден', 'Спарта'];
+// console.log(arrayFilms);
+// for (let film of arrayFilms){
+// console.log(film);
+// }
+for (let i = 0; i < arrayFilms.length; i++ ){
+    console.log(arrayFilms[i]);    
+}
+
+
+// 2 преобразовать массив в строку и обратно
+let arrayCar = ['BMW', 'Audi', 'Opel', 'Renault', 'Mazda', 'Mini'];
+let arraySt = arrayCar.join('; ');    
+console.log(arraySt);
+let arrayCars = arraySt.split('; ');    
+console.log(arrayCars);
+
+
+// 3 добавить к каждому hello
+let arrayName = ['Ksenia', 'Ivan', 'Mihail', 'Dmitri', 'Gleb', 'Ylia'];
+
+
+// 4 числовой массив в Boolean
+
+
+// 5 отсортировать по убыванию
+let arrayNum = [1, 6, 7, 8, 3, 4, 5, 6];
+
+// 6 отфильтровать по значению >3
+
+
+// 7 функция принимает массив и число и выводит индекс элемента
+
+
+// 8 цикл выводит число пока оно не станет меньше 10
+
+
+// 9 цикл для простых чисел
+
+
+// 10 цикл нечетных чисел
